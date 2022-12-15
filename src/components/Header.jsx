385e724de9061e80c06517ec74cccfa16f151275
archis <vazearchis@gmail.com>
2022-12-15 14:25:59 +0530

    fixed some css

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -21,7 +21,9 @@ export default function Header() {
                 style={{ justifyContent: state.isLoggedIn === true ? "space-between" : "center" }}
                 className="header-container">
                 <div style={{ top: state.alert[1] === true ? "90px" : "-20vh", backgroundColor: state.alert[2] === "error" ? "crimson" : "mediumseagreen" }} className="alert">{state.alert[0]}</div>
-                <h1 style={{ textAlign: "center" }} className="header-logo">MCQ-Ace</h1>
+                <h1 onClick={() => {
+                    window.open("https://mcqace.netlify.app/", "_self")
+                }} style={{ textAlign: "center", cursor: "pointer" }} className="header-logo">MCQ-Ace</h1>
                 <nav style={{ display: state.isLoggedIn === true ? "flex" : "none" }}>
                     <button
                         style={{ color: "black", fontWeight: "bold", fontSize: "12px", transform: state.tab === "My Quizes" ? "translateY(4px)" : "translateY(0)", boxShadow: state.tab === "My Quizes" ? "0 5px #ffffff33" : "0 9px #ffffff4d" }}
